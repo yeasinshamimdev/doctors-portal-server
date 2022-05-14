5aa17ab61df284c089c20a13edac140aa73e8fc7
Shamim <yeasinshamim7@gmail.com>
2022-05-14 06:59:05 +0600

    available service api done

--- index.js
+++ index.js
@@ -18,13 +18,47 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || "May 13, 2022";
+            // get all services
+            const services = await serviceCollection.find().toArray();
+
+            // get the booking on that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // for each service find booking for that service
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBookings.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+
+            res.send(bookings);
         })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists });
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        });
+
     }
     finally {
 
